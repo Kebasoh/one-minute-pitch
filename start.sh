84a17da94750ee0a14c4ade9b3c023157ca16a2b
kebasoh <ongatikebasoba@gmail.com>
2020-01-13 08:46:07 +0300

    editted the application

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
-export SECRET_KEY="kebasoh"
+export MOVIE_API_KEY='5ecabdbe419eeda80f605c702c18c68f'
+export SECRET_KEY='Yousecretkey'
+export MAIL_USERNAME='ongatikebaso@gmail.com'
+export MAIL_PASSWORD='1234'
+python manage.py server
 
-python3 manage.py server
